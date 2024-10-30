1f92792b1b6cca9dfeffd347644b4db3bfdf2659
Eduardomarinho19 <eduardomarinho0506@gmail.com>
2024-10-29 22:23:57 -0300

    Add files via upload

---
+++ teste.sql
@@ -0,0 +1,15 @@
+use teste;
+create table cliente
+(
+	codigo_cliente int,
+    nome_do_cliente varchar(255),
+    sobrenome varchar(255),
+    cpf int,
+    cep char(8),
+    telefone varchar(9)
+);
+create table funcionario 
+(
+codigo_funcionario int,
+nome_funcionario varchar(255),
+);
